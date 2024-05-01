becdddefef6335668b89781ff7cc6e4f94784a74
kolyasapphire <a@ks.gg>
2024-05-01 21:21:09 +0400

    Cookies

--- index.ts
+++ index.ts
@@ -40,7 +40,26 @@ const job = async () => {
     }
   };
 
-  const res = await fetch(`https://www.xbox.com/en-US/wishlist/${WISHLIST_ID}`);
+  const res = await fetch(
+    `https://www.xbox.com/en-US/wishlist/${WISHLIST_ID}`,
+    {
+      headers: {
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-GB,en;q=0.9",
+        Connection: "keep-alive",
+        Cookie:
+          "MUID=6E62D6A40F8044868A00E473B73CF8B5; MicrosoftApplicationsTelemetryDeviceId=f8162252-c2b1-4548-852a-6b8a1346291c; ai_session=2N7Uvd7PS2e7LMEnvlIKgM|1714583942031|1714583942503; aka_locale=en-us",
+        Host: "www.xbox.com",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15",
+      },
+    },
+  );
   const text = await res.text();
   const parsed = parse(text);
 
